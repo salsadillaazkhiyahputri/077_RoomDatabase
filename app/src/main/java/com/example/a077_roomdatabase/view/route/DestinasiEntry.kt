78f715d27dada257ae148e959afe081f07bb0130
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-11-27 14:36:02 +0700

    membuat DestinasiEntry untuk masuk ke halaman input siswa

---
+++ app/src/main/java/com/example/a077_roomdatabase/view/route/DestinasiEntry.kt
@@ -0,0 +1,10 @@
+package com.example.a077_roomdatabase.view.route
+
+import com.example.a077_roomdatabase.R
+import com.example.a077_roomdatabase.view.route.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+
+    override val titleRes = R.string.entry_siswa
+}
