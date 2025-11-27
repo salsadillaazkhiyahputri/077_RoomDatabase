48e3c21f72fc75da6274a214f26d89b74eca249b
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-11-27 14:44:03 +0700

    membuat  ViewModel agar Home & Entry bisa kebaca dari aplikasi

---
+++ app/src/main/java/com/example/a077_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.a077_roomdatabase.viewmodel.provider
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.a077_roomdatabase.repositori.AplikasiSiswa
+import com.example.a077_roomdatabase.viewmodel.EntryViewModel
+import com.example.a077_roomdatabase.viewmodel.HomeViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+        initializer {
+            EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+    }
+}
+
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
