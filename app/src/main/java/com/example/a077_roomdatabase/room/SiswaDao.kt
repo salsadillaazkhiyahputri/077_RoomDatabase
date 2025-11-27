d66f820159d177284a8d95ed00147b6184ea1923
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-11-27 15:06:22 +0700

    menambahkan   @Dao difilenya

--- app/src/main/java/com/example/a077_roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/a077_roomdatabase/room/SiswaDao.kt
@@ -1,10 +1,12 @@
 package com.example.a077_roomdatabase.room
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface SiswaDao {
     @Query("SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
