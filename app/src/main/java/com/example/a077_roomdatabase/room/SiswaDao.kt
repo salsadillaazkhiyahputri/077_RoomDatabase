a2628487bb5faba0cdd9160148482122f7b67019
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-11-27 14:34:15 +0700

    memperbaiki kesalahan pada SiswaDao.kt

--- app/src/main/java/com/example/a077_roomdatabase/room/SiswaDao.kt
+++ app/src/main/java/com/example/a077_roomdatabase/room/SiswaDao.kt
@@ -9,6 +9,6 @@ interface SiswaDao {
     @Query("SELECT * from tblSiswa ORDER BY nama ASC")
     fun getAllSiswa(): Flow<List<Siswa>>
 
-    @Insert(OnConflict = OnConflictStrategy.IGNORE)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insert(siswa: Siswa)
 }
