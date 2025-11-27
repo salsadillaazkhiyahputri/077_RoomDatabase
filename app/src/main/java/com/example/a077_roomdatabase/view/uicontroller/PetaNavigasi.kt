6de295021d8455d29980c70f85bb4a87a8c3e0cc
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-11-27 14:50:11 +0700

    memperbaiki import di PetaNavigasi.kt

--- app/src/main/java/com/example/a077_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a077_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -4,7 +4,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.a077_roomdatabase.view.EntrySiswaScreen
+import com.example.a077_roomdatabase.view.HomeScreen
+import com.example.a077_roomdatabase.view.route.DestinasiEntry
+import com.example.a077_roomdatabase.view.route.DestinasiHome
 
 @Composable
 fun SiswaApp(navController: NavHostController = rememberNavController(), modifier: Modifier){
