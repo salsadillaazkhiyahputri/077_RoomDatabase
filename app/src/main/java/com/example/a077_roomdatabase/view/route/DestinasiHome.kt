eb8e5b078ec42c99b35e4be2361f56589317bf2c
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-11-27 14:36:39 +0700

    membuat  DestinasiHome buat navigasi ke halaman utama

---
+++ app/src/main/java/com/example/a077_roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,12 @@
+package com.example.a077_roomdatabase.view.route
+
+import com.example.a077_roomdatabase.R
+import com.example.a077_roomdatabase.view.route.DestinasiNavigasi
+
+object DestinasiHome : DestinasiNavigasi{
+
+    override val route = "home"
+
+    override val titleRes = R.string.app_name
+
+}
