fdf1fc31478dae2f4941cee9c850ac5ff37be3bc
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-12-04 11:54:25 +0700

    menambahkan  idSiswa ke route detail

--- app/src/main/java/com/example/a077_roomdatabase/view/route/DestinasiDetailSiswa.kt
+++ app/src/main/java/com/example/a077_roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -3,6 +3,8 @@ package com.example.a077_roomdatabase.view.route
 import com.example.a077_roomdatabase.R
 
 object DestinasiDetailSiswa : DestinasiNavigasi {
-        override val route = "detail_siswa"
-        override val titleRes = R.string.detail_siswa
-
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
