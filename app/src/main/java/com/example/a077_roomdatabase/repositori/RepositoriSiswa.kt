c53145ed94c939c3bae7b323aa65c9c0967c520e
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-12-04 11:43:44 +0700

    menambahkan stream dan delete

--- app/src/main/java/com/example/a077_roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/a077_roomdatabase/repositori/RepositoriSiswa.kt
@@ -19,4 +19,6 @@ class OfflineRepositoriSiswa(
 ): RepositoriSiswa {
     override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
     override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+    override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
+    override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
 }
