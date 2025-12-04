f81ca37fd582f4f54edb0335f428578f453d3997
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-12-04 13:07:05 +0700

    memperbaiki kesalahan penulisan

--- app/src/main/java/com/example/a077_roomdatabase/repositori/RepositoriSiswa.kt
+++ app/src/main/java/com/example/a077_roomdatabase/repositori/RepositoriSiswa.kt
@@ -6,19 +6,22 @@ import kotlinx.coroutines.flow.Flow
 
 interface RepositoriSiswa {
     fun getAllSiswaStream(): Flow<List<Siswa>>
+    suspend fun insertSiswa(siswa: Siswa)
+
+    fun getSiswaStream(id: Int): Flow<Siswa?>
 
     suspend fun deleteSiswa(siswa: Siswa)
-
-
-    suspend fun insertSiswa(siswa: Siswa)
 
 }
 
 class OfflineRepositoriSiswa(
     private val siswaDao: SiswaDao
-): RepositoriSiswa {
-    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao.getAllSiswa()
-    override suspend fun insertSiswa(siswa: Siswa) = siswaDao.insert(siswa)
+): RepositoriSiswa{
+    override fun getAllSiswaStream(): Flow<List<Siswa>> = siswaDao
+        .getAllSiswa()
+    override suspend fun insertSiswa(siswa: Siswa) = siswaDao
+        .insert(siswa)
     override fun getSiswaStream(id: Int): Flow<Siswa?> = siswaDao.getSiswa(id)
     override suspend fun deleteSiswa(siswa: Siswa) = siswaDao.delete(siswa)
+
 }
