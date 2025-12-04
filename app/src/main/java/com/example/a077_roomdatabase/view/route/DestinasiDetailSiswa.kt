9545acff9026e3c7043f781d65d45ae254a009b0
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-12-04 11:53:27 +0700

    menambahkan route & title detail siswa

---
+++ app/src/main/java/com/example/a077_roomdatabase/view/route/DestinasiDetailSiswa.kt
@@ -0,0 +1,8 @@
+package com.example.a077_roomdatabase.view.route
+
+import com.example.a077_roomdatabase.R
+
+object DestinasiDetailSiswa : DestinasiNavigasi {
+        override val route = "detail_siswa"
+        override val titleRes = R.string.detail_siswa
+
