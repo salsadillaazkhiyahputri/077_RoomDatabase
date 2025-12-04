71a79f017865b39e5f6e848ac5f1caf167b3cd4f
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-12-04 13:08:06 +0700

    menambahkan  DetailViewModel lewat savedState & repo

--- app/src/main/java/com/example/a077_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/a077_roomdatabase/viewmodel/provider/PenyediaViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.a077_roomdatabase.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.a077_roomdatabase.repositori.AplikasiSiswa
+import com.example.a077_roomdatabase.viewmodel.DetailViewModel
 import com.example.a077_roomdatabase.viewmodel.EntryViewModel
 import com.example.a077_roomdatabase.viewmodel.HomeViewModel
 
@@ -13,11 +15,15 @@ object PenyediaViewModel {
         initializer {
             HomeViewModel(aplikasiSiswa().container.repositoriSiswa)
         }
+
         initializer {
             EntryViewModel(aplikasiSiswa().container.repositoriSiswa)
+        }
+
+        initializer {
+            DetailViewModel(this.createSavedStateHandle(), aplikasiSiswa().container.repositoriSiswa)
         }
     }
 }
 
-fun CreationExtras.aplikasiSiswa(): AplikasiSiswa =
-    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
+fun CreationExtras.aplikasiSiswa(): AplikasiSiswa = (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiSiswa)
